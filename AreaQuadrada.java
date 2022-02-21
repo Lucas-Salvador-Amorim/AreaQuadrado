fa620ccde8075301f81b47a72c08b2468164752b
Lucas Salvador Amorim <lucassalvadoramorim@gmail.com>
2022-02-21 19:47:30 +0000

    Testando

---
+++ AreaQuadrada.java
@@ -0,0 +1,7 @@
+public class AreaQuadrada {
+    public static void main(String[] args){
+        double media = 7.55;
+        System.out.printf("Média = %.2f", media);
+        System.out.println("Testando");
+    }   
+}
