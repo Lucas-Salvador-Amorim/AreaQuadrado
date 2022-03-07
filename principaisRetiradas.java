a299630bcf846cd0aa7bfe69759efa7a01f1f068
Lucas Salvador Amorim <lucassalvadoramorim@gmail.com>
2022-03-07 16:29:48 +0000

    principaisRetiradas

---
+++ principaisRetiradas.java
@@ -0,0 +1,13 @@
+public class principaisRetiradas{
+    public static void main(String[] args){
+
+        int primeiraSemana = 4;
+        int segundaSemana = 5;
+        int terceiraSemana = 2;
+        int quartaSemana = 7;
+
+        int totalMes = primeiraSemana + segundaSemana + terceiraSemana + quartaSemana;
+
+        System.out.println(totalMes);
+    }
+}
